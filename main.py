3c58e346b512ef93c55268150503d4761aa7222e
Niksa Visic <niksavis@live.com>
2025-04-02 15:08:18 +0200

    Renamed main.py in app folder to app.py to avoid confusion

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Resource Management Application Entry Point
 """
 
 import streamlit as st
-from app.main import main
+from app.app import main
 
 if __name__ == "__main__":
     st.set_page_config(page_title="Resource Management App", layout="wide")
